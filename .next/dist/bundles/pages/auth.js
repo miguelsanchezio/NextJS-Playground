63bd3d87ff1373e9aef5feda73123b5cbaa9237a
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-25 14:26:24 -0700

    Styled User component using next jsx.

--- .next/dist/bundles/pages/auth.js
+++ .next/dist/bundles/pages/auth.js
@@ -76,23 +76,28 @@ module.exports =
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("react");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_styled_jsx_style__ = __webpack_require__("styled-jsx/style");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_styled_jsx_style___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_styled_jsx_style__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react__ = __webpack_require__("react");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react__);
 var _jsxFileName = '/Users/miguelsanchez/Websites/Udemy/React 16 - The Complete Guide/NextjsPlayground/components/User.js';
 
 
+
 var User = function User(props) {
-  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+  return __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
     'div',
     {
+      className: 'jsx-2974957094',
       __source: {
         fileName: _jsxFileName,
         lineNumber: 4
       }
     },
-    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+    __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
       'h1',
       {
+        className: 'jsx-2974957094',
         __source: {
           fileName: _jsxFileName,
           lineNumber: 5
@@ -100,9 +105,10 @@ var User = function User(props) {
       },
       props.name
     ),
-    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+    __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(
       'p',
       {
+        className: 'jsx-2974957094',
         __source: {
           fileName: _jsxFileName,
           lineNumber: 6
@@ -110,7 +116,11 @@ var User = function User(props) {
       },
       'Age: ',
       props.age
-    )
+    ),
+    __WEBPACK_IMPORTED_MODULE_1_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_0_styled_jsx_style___default.a, {
+      styleId: '2974957094',
+      css: 'div.jsx-2974957094{border:1px solid #eee;box-shadow:0 2px 3px #ccc;padding:20px;text-align:center;width:400px;}\n/*# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvVXNlci5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFNZ0IsQUFHK0Isc0JBQ0ksMEJBQ2IsYUFDSyxrQkFDTixZQUNkIiwiZmlsZSI6ImNvbXBvbmVudHMvVXNlci5qcyIsInNvdXJjZVJvb3QiOiIvVXNlcnMvbWlndWVsc2FuY2hlei9XZWJzaXRlcy9VZGVteS9SZWFjdCAxNiAtIFRoZSBDb21wbGV0ZSBHdWlkZS9OZXh0anNQbGF5Z3JvdW5kIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuY29uc3QgVXNlciA9IHByb3BzID0+IChcbiAgPGRpdj5cbiAgICA8aDE+e3Byb3BzLm5hbWV9PC9oMT5cbiAgICA8cD5BZ2U6IHtwcm9wcy5hZ2V9PC9wPlxuICAgIDxzdHlsZSBqc3g+e2BcbiAgICAgIGRpdiB7XG4gICAgICAgIGJvcmRlcjogMXB4IHNvbGlkICNlZWU7XG4gICAgICAgIGJveC1zaGFkb3c6IDAgMnB4IDNweCAjY2NjO1xuICAgICAgICBwYWRkaW5nOiAyMHB4O1xuICAgICAgICB0ZXh0LWFsaWduOiBjZW50ZXI7XG4gICAgICAgIHdpZHRoOiA0MDBweDtcbiAgICAgIH1cbiAgICBgfTwvc3R5bGU+XG4gIDwvZGl2PlxuKTtcblxuZXhwb3J0IGRlZmF1bHQgVXNlcjsiXX0= */\n/*@ sourceURL=components/User.js */'
+    })
   );
 };
 
@@ -173,6 +183,13 @@ module.exports = __webpack_require__("./pages/auth/index.js");
 /***/ (function(module, exports) {
 
 module.exports = require("react");
+
+/***/ }),
+
+/***/ "styled-jsx/style":
+/***/ (function(module, exports) {
+
+module.exports = require("styled-jsx/style");
 
 /***/ })
 
